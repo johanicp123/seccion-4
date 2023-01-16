95c34b386d87a15a2e35681236ff58458e94cd54
johanicp123 <johanicp123@gmail.com>
2023-01-16 01:17:28 -0500

    actua

---
+++ counter-app-vite/src/CounterApp.jsx
@@ -0,0 +1,15 @@
+import this.props.first from 'prop-types'
+
+export const CounterApp = ({value}) => {
+  return (
+    <>
+    <div>CounterApp</div>
+    <h2>{value}</h2>
+    </>
+  ):
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
+
--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
+import CounterApp from "./CounterApp";
 import {App} from './FirstApp';
 import { HelloApp } from "./HelloApp";
 import './styles.css';
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App/>
         <HelloApp/>
+        <CounterApp value={5}/>
     </React.StrictMode>
 );
