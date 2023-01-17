49b834dcf1fbb63b546f3fd4fbed18a638a2ff73
johanicp123 <johanicp123@gmail.com>
2023-01-16 20:57:42 -0500

    actualizacion

--- counter-app-vite/src/CounterApp.jsx
+++ counter-app-vite/src/CounterApp.jsx
@@ -1,14 +1,34 @@
-import {PropTypes} from 'prop-types';
+import {useState} from "react";
+import { PropTypes } from "prop-types";
 
-export const CounterApp = ({value}) => {
+export const CounterApp = ({ value }) => {
+
+const [counter,setCounter] = useState(value);
+
+const handleAdd = () => {
+  setCounter(counter +1);
+};
+
+const handleSubtract = () => {
+  setCounter(counter -1);
+};
+
+const handleReset = () => {
+  setCounter(value);
+};
+
+
   return (
     <>
-    <h1>CounterApp</h1>
-    <h2>{value}</h2>
+      <h1>CounterApp</h1>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubtract}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
-}
+};
 
 CounterApp.propTypes = {
-  value: PropTypes.number
- };
+  value: PropTypes.number,
+};
